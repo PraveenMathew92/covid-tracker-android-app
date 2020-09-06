efa27a5b7f1f11a69e697419b19b08ce1dca7f01
Praveen Mathew <praveen.mathew92@outlook.com>
2020-09-06 19:02:11 +0530

    Show message on button click

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myfirstandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startVideoButton = (Button) findViewById(R.id.startVideoButton);
+        startVideoButton.setOnClickListener(new View.OnClickListener () {
+
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                String toastMessage = "Clicked Start Video Button";
+                int duration =  Toast.LENGTH_SHORT;
+                Toast.makeText(context, toastMessage, duration)
+                    .show();
+            }
+        });
     }
 }
