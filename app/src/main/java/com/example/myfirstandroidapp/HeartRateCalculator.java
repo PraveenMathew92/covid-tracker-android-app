a93f3d87ce029e811efb0dae7dcc47aa4761e74f
Praveen Mathew <praveen.mathew92@outlook.com>
2020-09-23 19:40:00 +0530

    Refactor Heart Rate Service

--- app/src/main/java/com/example/myfirstandroidapp/HeartRateCalculator.java
+++ app/src/main/java/com/example/myfirstandroidapp/HeartRateCalculator.java
@@ -2,47 +2,29 @@ package com.example.myfirstandroidapp;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Binder;
 import android.os.IBinder;
+
 import androidx.annotation.Nullable;
+
+import com.arthenica.mobileffmpeg.FFmpeg;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.videoio.VideoCapture;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executors;
 
+import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_SUCCESS;
+
 public class HeartRateCalculator extends Service {
-    static double calculate(String filePath) {
-        VideoCapture videoCapture = new VideoCapture(filePath);
-        if (!videoCapture.isOpened()) {
-            return -1;
-        }
-        List<Double> meanRedIntensities = new ArrayList<>();
-        Mat image = new Mat();
-        Mat redChannel = new Mat();
-        while (videoCapture.read(image)) {
-            videoCapture.read(image);
-            Core.extractChannel(image, redChannel, 2);
-            double frameRedIntensityMean = Core.mean(redChannel).val[0];
-            meanRedIntensities.add(frameRedIntensityMean);
-        }
+    static double calculate(List<Double> meanRedIntensities) {
+
         List<Double> movingAverageList = movingAverage(meanRedIntensities, 5, 5);
-
-        int sampleCount = 9;
-        int sampleSize = movingAverageList.size()/sampleCount;
-        int zeroCrossOverCount = 0;
-//        for(int i = sampleSize; i < movingAverageList.size(); i += sampleSize){
-//            int zeroCrossOverInSample = zeroCrossOvers(movingAverageList.subList(i - sampleSize, i));
-//            System.out.println(zeroCrossOverInSample);
-//            zeroCrossOverCount += zeroCrossOverInSample;
-//        }
-        zeroCrossOverCount = zeroCrossOvers(movingAverageList);
-        double averageZeroCrossOvers = (double) (zeroCrossOverCount)/sampleCount;
-
-//        average * 1/2 * sampleCount *  3/4
-
-        double heartRate = (double) (zeroCrossOverCount * 3)/8;
+        int zeroCrossOverCount = zeroCrossOvers(movingAverageList);
+        double heartRate = (double) (zeroCrossOverCount * 3) / 8;
         return heartRate;
     }
 
@@ -62,7 +44,7 @@ public class HeartRateCalculator extends Service {
     }
 
     public static int zeroCrossOvers(List<Double> list) {
-        if(list.size() < 2)
+        if (list.size() < 2)
             return 0;
         double previousElement;
         double nextElement = list.get(1);
@@ -82,11 +64,13 @@ public class HeartRateCalculator extends Service {
 
     @Override
     public int onStartCommand(final Intent intent, int flags, int startId) {
+        convertCapturedImage();
         Executors.newSingleThreadExecutor().execute(new Runnable() {
             @Override
             public void run() {
                 String path = "/storage/emulated/0/Android/data/com.example.myfirstandroidapp/files/FingertipVideo.avi";
-                double heartRate = HeartRateCalculator.calculate(path);
+                List<Double> averageRedIntensityPerFrame = calculateMeanRedIntensities(path);
+                double heartRate = HeartRateCalculator.calculate(averageRedIntensityPerFrame);
                 Intent broadcastIntent = new Intent();
                 broadcastIntent.setAction("Heart Rate Receive Action");
                 broadcastIntent.putExtra("heartRate", heartRate);
@@ -96,18 +80,35 @@ public class HeartRateCalculator extends Service {
         return START_REDELIVER_INTENT;
     }
 
-//    @Override
-//    public void onDestroy() {
-//        super.onDestroy();
-//        Intent broadcastIntent = new Intent();
-//        broadcastIntent.setAction("restartservice");
-//        broadcastIntent.setClass(this, HeartRateCalculator.class);
-//        this.sendBroadcast(broadcastIntent);
-//    }
+    private List<Double> calculateMeanRedIntensities(String filePath) {
+        VideoCapture videoCapture = new VideoCapture(filePath);
+        if (!videoCapture.isOpened()) {
+            return Collections.emptyList();
+        }
+        List<Double> meanRedIntensities = new ArrayList<>();
+        Mat image = new Mat();
+        Mat redChannel = new Mat();
+        while (videoCapture.read(image)) {
+            videoCapture.read(image);
+            Core.extractChannel(image, redChannel, 2);
+            double frameRedIntensityMean = Core.mean(redChannel).val[0];
+            meanRedIntensities.add(frameRedIntensityMean);
+        }
+        return meanRedIntensities;
+    }
 
-    public class HeartRateBinder extends Binder {
-        HeartRateCalculator getService() {
-            return HeartRateCalculator.this;
+    private void convertCapturedImage() {
+        String capturedImageName = "/index-finger-video";
+        String capturedImageLocation = getApplicationContext().getExternalFilesDirs(null)[0] + capturedImageName;
+        int conversionOneReturnCode = FFmpeg.execute("-i " + capturedImageLocation
+                + ".mp4 -vcodec mjpeg " + capturedImageLocation + ".mjpeg");
+        int conversionTwoReturnCode = FFmpeg.execute("-i " + capturedImageLocation
+                + ".mjpeg -vcodec mjpeg " + capturedImageLocation + ".avi");
+
+        if (conversionOneReturnCode == RETURN_CODE_SUCCESS && conversionTwoReturnCode == RETURN_CODE_SUCCESS) {
+            System.out.println("Image Converted Successfully");
+        } else {
+            System.out.println("Image Conversion Failed");
         }
     }
 
