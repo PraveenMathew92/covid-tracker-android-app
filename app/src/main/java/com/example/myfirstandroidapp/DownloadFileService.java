7911588349410ab9722094700132b10a83d55833
Praveen Mathew <praveen.mathew92@outlook.com>
2020-11-08 00:42:54 +0530

    Display contact graph

--- app/src/main/java/com/example/myfirstandroidapp/DownloadFileService.java
+++ app/src/main/java/com/example/myfirstandroidapp/DownloadFileService.java
@@ -1,9 +1,12 @@
 package com.example.myfirstandroidapp;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.os.IBinder;
 import androidx.annotation.Nullable;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -23,8 +26,13 @@ public class DownloadFileService extends Service {
     public int onStartCommand(final Intent intent, int flags, int startId) {
         String subjectId = intent.getStringExtra("subject_id");
         String date = intent.getStringExtra("date");
-        Objects.requireNonNull(date).replace("-", "");
+        date = Objects.requireNonNull(date).replace("-", "");
         final String urlString = "http://10.0.2.2:5000/" + subjectId + "/" + date;
+        final String contactGraphFileName = "ContactGraph";
+        File file = new File(getApplicationContext().getFilesDir(), contactGraphFileName); //delete if exists
+        if (file.exists()) {
+            file.delete();
+        }
 
         Executors.newSingleThreadExecutor().execute(new Runnable() {
             @Override
@@ -41,7 +49,7 @@ public class DownloadFileService extends Service {
                     urlConnection.setRequestProperty("X-Environment", "android");
                     urlConnection.connect();
                     input = urlConnection.getInputStream();
-                    OutputStream output = getApplicationContext().openFileOutput("ContactGraph", MODE_PRIVATE);
+                    OutputStream output = getApplicationContext().openFileOutput(contactGraphFileName, MODE_PRIVATE);
                     byte[] buffer = new byte[1024];
                     int bytesRead;
                     while ((bytesRead = input.read(buffer, 0, buffer.length)) >= 0) {
@@ -49,6 +57,7 @@ public class DownloadFileService extends Service {
                     }
                     output.close();
                     input.close();
+                    getApplicationContext().sendBroadcast(new Intent("Contact Graph File Downloaded"));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
--- app/src/main/java/com/example/myfirstandroidapp/GetContactGraphActivity.java
+++ app/src/main/java/com/example/myfirstandroidapp/GetContactGraphActivity.java
@@ -1,11 +1,12 @@
 package com.example.myfirstandroidapp;
 
 import android.app.DatePickerDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.DrawableContainer;
-import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -16,16 +17,83 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-import androidx.annotation.ArrayRes;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class GetContactGraphActivity extends AppCompatActivity {
+    private BroadcastReceiver fileDownloadReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            FileInputStream contactGraphFile =  null;
+            try {
+                contactGraphFile = getApplicationContext().openFileInput("ContactGraph");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return;
+            }
+            Scanner scanner = new Scanner(contactGraphFile);
+
+            TableLayout tableLayout = (TableLayout) findViewById(R.id.contact_graph);
+            tableLayout.removeAllViews();
+            TableRow tableRowHeader = new TableRow(getApplicationContext());
+            {
+                TextView textView = new TextView(getApplicationContext());
+                textView.setText("subject_id");
+                textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
+                textView.setPadding(10, 2, 10, 2);
+                textView.setBackgroundResource(R.drawable.cell_shape);
+                tableRowHeader.addView(textView);
+            }
+            String[] subjectIds = getResources().getStringArray(R.array.subject_array);
+            for(String subjectId : subjectIds) {
+                TextView textView = new TextView(getApplicationContext());
+                textView.setText(subjectId);
+                textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
+                textView.setPadding(10, 2, 10, 2);
+                textView.setBackgroundResource(R.drawable.cell_shape);
+                textView.setGravity(View.TEXT_ALIGNMENT_CENTER);
+                tableRowHeader.addView(textView);
+            }
+            tableLayout.addView(tableRowHeader);
+
+            for(String subjectId : subjectIds) {
+                TableRow tableRow = new TableRow(getApplicationContext());
+                {
+                    TextView textView = new TextView(getApplicationContext());
+                    textView.setText(subjectId);
+                    textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
+                    textView.setPadding(10, 2, 10, 2);
+                    textView.setBackgroundResource(R.drawable.cell_shape);
+                    textView.setGravity(View.TEXT_ALIGNMENT_CENTER);
+                    tableRow.addView(textView);
+                }
+                for(String contactSubjectId : subjectIds) {
+                    String adjacency = scanner.next();
+                    TextView textView = new TextView(getApplicationContext());
+                    textView.setText(adjacency);
+                    textView.setPadding(10, 2, 10, 2);
+                    textView.setBackgroundResource(R.drawable.cell_shape);
+                    if(adjacency.equals("1")){
+                        textView.setTextColor(Color.RED);
+                    }
+                    textView.setGravity(View.TEXT_ALIGNMENT_CENTER);
+                    tableRow.addView(textView);
+                }
+                tableLayout.addView(tableRow);
+            }
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.get_contact_graph);
 
+        getApplicationContext().registerReceiver(fileDownloadReceiver, new IntentFilter("Contact Graph File Downloaded"));
         final TextView inputDate = findViewById(R.id.selected_date);
 
         final Button selectDateButton = (Button) findViewById(R.id.select_date_button);
@@ -33,7 +101,7 @@ public class GetContactGraphActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 int year = 2011;
-                int month = 11;
+                int month = 06;
                 int day = 15;
 
                 DatePickerDialog datePickerDialog = new DatePickerDialog(GetContactGraphActivity.this,
@@ -41,7 +109,8 @@ public class GetContactGraphActivity extends AppCompatActivity {
                             @Override
                             public void onDateSet(DatePicker view, int year,
                                                   int monthOfYear, int dayOfMonth) {
-                                inputDate.setText(String.format("%d-%d-%d", year, monthOfYear + 1, dayOfMonth));
+                                inputDate.setText(String.format("%4d-%2d-%2d", year, monthOfYear + 1, dayOfMonth)
+                                        .replace(' ', '0'));
                             }
                         }, year, month, day);
                 datePickerDialog.show();
@@ -58,43 +127,12 @@ public class GetContactGraphActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ((TableLayout) findViewById(R.id.contact_graph)).removeAllViews();
                 Intent intent = new Intent(getApplicationContext(), DownloadFileService.class);
                 intent.putExtra("subject_id", spinner.getSelectedItem().toString());
                 intent.putExtra("date", inputDate.getText().toString());
                 startService(intent);
             }
         });
-
-        TableLayout tableLayout = (TableLayout) findViewById(R.id.contact_graph);
-        TableRow tableRowHeader = new TableRow(this);
-        {
-            TextView textView = new TextView(this);
-            textView.setText("subject_id");
-            textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
-            textView.setPadding(10, 2, 10, 2);
-            textView.setBackgroundResource(R.drawable.cell_shape);
-            tableRowHeader.addView(textView);
-        }
-        for(String subject_id : getResources().getStringArray(R.array.subject_array)) {
-            TextView textView = new TextView(this);
-            textView.setText(subject_id);
-            textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
-            textView.setPadding(10, 2, 10, 2);
-            textView.setBackgroundResource(R.drawable.cell_shape);
-            tableRowHeader.addView(textView);
-        }
-        tableLayout.addView(tableRowHeader);
-
-        for(int i = 1; i<5; i++) {
-            TableRow tableRow = new TableRow(this);
-            for(int j = 1; j<5; j++) {
-                TextView textView = new TextView(this);
-                textView.setText(Integer.toString(i*10 + j));
-                textView.setPadding(10, 2, 10, 2);
-                textView.setBackgroundResource(R.drawable.cell_shape);
-                tableRow.addView(textView);
-            }
-            tableLayout.addView(tableRow);
-        }
     }
 }
